d8f4f4b39a8e7d5298d5f12dbde77abdd93546c2
Andrew Jreij <andrew.jreij@gmail.com>
2023-02-05 18:41:56 -0500

    removed all references to person

--- utils/config.js
+++ utils/config.js
@@ -3,10 +3,8 @@ require('dotenv').config()
 const PORT = process.env.PORT
 
 const MONGODB_URI = process.env.MONGODB_URI
-const MONGODB_URI_PERSON = process.env.MONGODB_URI_PERSON
 
 module.exports = {
     MONGODB_URI,
-    MONGODB_URI_PERSON,
     PORT
 }
