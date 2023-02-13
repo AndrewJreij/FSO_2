4146f434bed53fb880f3a08ca65c6910f67df8c3
Andrew Jreij <andrew.jreij@gmail.com>
2023-02-12 23:41:40 -0500

    exercises 4.8 - 11

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 
-const mongoUrl = process.env.MONGODB_URI_BLOG
+const mongoUrl = process.env.NODE_ENV === 'test'
+    ? process.env.MONGODB_URI_BLOG_TEST
+    : process.env.MONGODB_URI_BLOG
+
 const PORT = process.env.PORT_BLOG
 
 module.exports = {
