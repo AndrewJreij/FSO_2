9a4747c52992ab285ae411ed8b433b94864c73d3
Andrew Jreij <andrew.jreij@gmail.com>
2023-02-12 21:44:40 -0500

    testing

--- mongo.js
+++ mongo.js
@@ -1,55 +1,34 @@
 const mongoose = require('mongoose')
+const config = require('./utils/config')
 
-const password = process.argv[2]
-
-const url = `mongodb+srv://fullstack:${password}@cluster0.ulhx4mv.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+const url = 'asd'
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
-const numberSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+const noteSchema = new mongoose.Schema({
+    content: String,
+    date: Date,
+    important: Boolean,
 })
 
-const Number = mongoose.model('Number', numberSchema)
+const Note = mongoose.model('Note', noteSchema)
 
-if (process.argv.length === 5) {
-    const name = process.argv[3]
-    const number = process.argv[4]
+const note = new Note({
+    content: 'CSS is hard',
+    date: new Date(),
+    important: true,
+})
 
-    const newNumber = new Number({
-        name: name,
-        number: number,
-    })
+note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+})
+/*
 
-    newNumber.save().then(result => {
-        console.log('number saved!')
-        mongoose.connection.close()
-    })
-
-} else if (process.argv.length === 3) {
-    Number.find({}).then(result => {
-        result.forEach(number => {
-            console.log(`${number.name} ${number.number}`)
-        })
-
-        mongoose.connection.close()
-    })
-}
-
-
-
-// Note.find({}).then(result => {
-//     result.forEach(note => {
-//         console.log(note)
-//     })
-//     mongoose.connection.close()
-// })
-
-// Note.find({ important: true }).then(result => {
-//     result.forEach(note => {
-//         console.log(note)
-//     })
-//     mongoose.connection.close()
-// })
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})*/
---
+++ tests/teardown.js
@@ -0,0 +1,5 @@
+// https://fullstackopen.com/en/part4/testing_the_backend
+// https://stackoverflow.com/questions/50687592/jest-and-mongoose-jest-has-detected-opened-handles
+module.exports = () => {
+    process.exit(0)
+}
